a97121128b78a075a2ca1501bd3d34b9693532df
Nico Mike Luethi <nluethi@siepc-08.epfl.ch>
2025-05-21 18:45:47 +0200

    [ODOMETRY] Added new plot

--- scripts/matlab/debug_scripts/plot_odo_acc.m
+++ scripts/matlab/debug_scripts/plot_odo_acc.m
@@ -32,6 +32,17 @@ xlabel('Time [s]'); ylabel('x [m]');
 %y_lim = [min([data.odo_acc_x;  data.pose_x]),max([data.odo_acc_x;  data.pose_x])];
 %xlim([data.time(1), data.time(end)]);ylim(y_lim + [-0.05,0.05]*(y_lim(2)-y_lim(1)));
 
+%% Plot position using accelerometer
+
+f = figure('Name','Webots : Odometry using accelerometer[m/s^2]');
+
+% Plot x : odometry vs ground truth (gps)
+plot(truth_data.x, truth_data.y); hold on;
+plot(odo_data.x, odo_data.y);
+title("x trajectory : odometry vs ground truth (gps)");
+legend("Ground Thruth : GPS", "Odometry : Accelerometer");
+xlabel('x [m]'); ylabel('y [m]');
+
 %% Plot the odometry heading computed using the accelerometer on Webots
 
 % ROBOT acc mean : -0.00148328 -0.00236683 9.49705
