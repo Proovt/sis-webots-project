b335f57ba7c14833865fb9a543732b2d6aed4b69
Nico Mike Luethi <nluethi@siepc-37.epfl.ch>
2025-05-28 16:03:49 +0200

    [FSM] New Odometry logic by Linus

--- controllers/controller/FSM.hpp
+++ controllers/controller/FSM.hpp
@@ -1,41 +1,163 @@
-#pragma once 
+#pragma once
 
-/**
- * TODO: Implement your FSM here
- * 
- * This file implements a very basic FSM that initially lets the robot go forward
- * and stops it when an obstacle is detected.
- * 
- * You can modify this file to implement your own FSM and implement your own behaviors.
-*/
+#include "pioneer_interface/pioneer_interface.hpp"
+#include "braitenberg.hpp"
+#include <iostream>
+#include <cmath>
 
-#include "pioneer_interface/pioneer_interface.hpp" // contains the NUM_SENSORS constant 
-#include "braitenberg.hpp" // you may want to use the braitenberg function to control the robot in some cases
-
-#define THRESHOLD   1010  // Arbitrary threshold value for the proximity sensors
-
+#define VERBOSE_STATE_NAME false
 
 //////////////////////
 // Global variables //
 //////////////////////
 
-// enum used to store the current behavior
-enum basicBehaviors {GOFORWARD, STOP} behavior;
+enum RobotState
+{
+  NAVIGATE,
+  TURNING,
+  EMERGENCY_BACKUP,
+  STRAIGHT,
+  STOP
+};
+
+static RobotState state = NAVIGATE;
+static RobotState prev_state = NAVIGATE;
+
+const std::string getStateName(RobotState s)
+{
+  switch (s)
+  {
+  case NAVIGATE:
+    return "NAVIGATE";
+  case TURNING:
+    return "TURNING";
+  case EMERGENCY_BACKUP:
+    return "EMERGENCY_BACKUP";
+  case STRAIGHT:
+    return "STRAIGHT";
+  case STOP:
+    return "STOP";
+  default:
+    return "UNKNOWN";
+  }
+}
 
 ////////////////////////
 // Behavior Functions //
 ////////////////////////
 
-void goForwardBehavior(double* ps_values, double &vel_left, double &vel_right){
-  vel_left = 1.0;
-  vel_right = 1.0;
-  return;
+double clamp(double val, double min_val, double max_val)
+{
+  return std::max(min_val, std::min(val, max_val));
 }
 
-void stopBehavior(double &vel_left, double &vel_right){
+void navigateBehavior(double *ps, double &vel_left, double &vel_right, double pose[4])
+{
+  braitenberg(ps, vel_left, vel_right);
+}
+
+void emergencyBackupBehavior(double &vel_left, double &vel_right)
+{
+  vel_left = -0.5;
+  vel_right = -0.5;
+}
+
+void straightBehavior(double &vel_left, double &vel_right)
+{
+  vel_left = 0.5;
+  vel_right = 0.5;
+}
+
+void stopBehavior(double &vel_left, double &vel_right)
+{
   vel_left = 0;
   vel_right = 0;
-  return;
+}
+
+void turningBehavior(
+    double *ps,
+    double &vel_left,
+    double &vel_right,
+    double pose[4],
+    bool turn_left,
+    bool turn_right,
+    bool pot_found,
+    bool &search_pot,
+    int &search_pot_counter,
+    const int search_pot_duration)
+{
+  double h = pose[2];
+  if (turn_left)
+  {
+    double target = 1000;
+    double sensor_left = std::max(ps[0], ps[15]);
+    double error = sensor_left - target;
+    double Kp = 0.015;
+    double correction = Kp * error;
+
+    if (pot_found && sensor_left < 850)
+    {
+      search_pot = true;
+    }
+
+    if (search_pot)
+    {
+      vel_left = 1;
+      vel_right = 1;
+      search_pot_counter++;
+      if (search_pot_counter > search_pot_duration)
+      {
+        search_pot = false;
+        search_pot_counter = 0;
+      }
+    }
+    else if (ps[0] < ps[15])
+    {
+      vel_left = clamp(0.2 + correction / 2, -3.0, 3.0);
+      vel_right = clamp(0.7 - correction, -3.0, 3.0);
+    }
+    else if (ps[0] > ps[15])
+    {
+      vel_left = 0.3;
+      vel_right = clamp(0.5 - correction, -3.0, 3.0);
+    }
+  }
+
+  if (turn_right)
+  {
+    double target = 1000;
+    double sensor_right = std::max(ps[7], ps[8]);
+    double error = sensor_right - target;
+    double Kp = 0.015;
+    double correction = Kp * error;
+
+    if (pot_found && sensor_right < 850)
+    {
+      search_pot = true;
+    }
+
+    if (search_pot)
+    {
+      vel_left = 1;
+      vel_right = 1;
+      search_pot_counter++;
+      if (search_pot_counter > search_pot_duration)
+      {
+        search_pot = false;
+        search_pot_counter = 0;
+      }
+    }
+    else if (ps[7] < ps[8])
+    {
+      vel_right = clamp(0.2 + correction / 2, -3.0, 3.0);
+      vel_left = clamp(0.7 - correction, -3.0, 3.0);
+    }
+    else if (ps[7] > ps[8])
+    {
+      vel_right = 0.3;
+      vel_left = clamp(0.5 - correction, -3.0, 3.0);
+    }
+  }
 }
 
 ///////////////////////
@@ -47,36 +169,190 @@ void stopBehavior(double &vel_left, double &vel_right){
  * @param ps_values array of proximity values from the robot's proximity sensors
  * @param[out] vel_lef left wheels velocity
  * @param[out] vel_right right wheels velocity
-*/
-void fsm(double* ps_values, double &vel_left, double &vel_right){
+ */
+bool fsm(double *ps, double &vel_left, double &vel_right, double pose[4])
+{
+  static int backup_counter = 0;
+  static bool is_emergency = false;
+  static bool turn_right = false;
+  static bool turn_left = false;
+  static bool is_straight = false;
+  static bool is_end_of_corridor = false;
+  static bool pot_found = false;
+  static int search_pot_counter = 0;
+  static bool search_pot = false;
+  static int stop_counter = 0;
 
-  switch(behavior){
+  const int backup_duration = 70;
+  const int search_pot_duration = 80;
+  const int stop_confirm_duration = 150;
 
-      case GOFORWARD:
-        
-        // Perform the goForward behavior
-        goForwardBehavior(ps_values, vel_left, vel_right);
+  double x = pose[0];
+  double y = pose[1];
+  double h = pose[2];
 
-        // Check for transition criteria 
-        for(int i=0; i<NUM_SENSORS; i++){
-          if (ps_values[i]>THRESHOLD){
-            printf("Obstacle detected, stopping...\n");
-            behavior = STOP;
-          }
-        }
-        break;
+  turn_left = x > 5;
+  turn_right = x < 0.0;
+  bool at_final_stop = (y > 3.5 && y < 4.5 && x < 0);
 
-      case STOP:
-
-        // Perform the stop behavior
-        stopBehavior(vel_left, vel_right);
-
-        // No transition criteria, the robot stays in this state forever
-        break;
-
-      default:
-        printf("This behavior is not implemented.\n");
-        break;
+  if (at_final_stop)
+  {
+    stop_counter++;
+    if (stop_counter > stop_confirm_duration)
+    {
+      state = STOP;
+      std::cout << "Stopped at the end of last corridor!" << std::endl;
+      return true;
     }
-    return;
+  }
+  else
+  {
+    stop_counter = 0;
+  }
+
+  // EMERGENCY BACKUP
+  if (is_emergency)
+  {
+    state = EMERGENCY_BACKUP;
+    backup_counter++;
+    bool rear_blocked = false;
+    for (int i = 9; i <= 11; ++i)
+    {
+      if (ps[i] > 1010)
+      {
+        rear_blocked = true;
+        break;
+      }
+    }
+    if (rear_blocked || backup_counter > backup_duration)
+    {
+      state = prev_state;
+      backup_counter = 0;
+      is_emergency = false;
+    }
+  }
+  else
+  {
+    for (int i = 1; i <= 6; ++i)
+    {
+      if (ps[i] > 1005)
+      {
+        prev_state = state;
+        state = EMERGENCY_BACKUP;
+        is_emergency = true;
+        break;
+      }
+    }
+  }
+
+  // STRAIGHT CORRIDOR
+  if (is_straight)
+  {
+    state = STRAIGHT;
+    backup_counter++;
+    if (backup_counter > backup_duration)
+    {
+      state = prev_state;
+      backup_counter = 0;
+      is_straight = false;
+    }
+  }
+  else if (ps[15] > 1010 || ps[8] > 1010)
+  {
+    prev_state = state;
+    state = STRAIGHT;
+    is_straight = true;
+  }
+
+  // TURN DETECTION (LEFT)
+  if (turn_left)
+  {
+    if (is_end_of_corridor)
+    {
+      pot_found = pot_found || ps[0] > 800 || ps[15] > 800;
+      if (h > 2.8)
+      {
+        is_end_of_corridor = false;
+        state = NAVIGATE;
+        pot_found = false;
+      }
+    }
+    else
+    {
+      bool out_cor = true;
+      for (int i = 0; i <= 7; ++i)
+      {
+        if (ps[i] > 850)
+        {
+          out_cor = false;
+          break;
+        }
+      }
+      if (out_cor && !at_final_stop)
+      {
+        state = TURNING;
+        is_end_of_corridor = true;
+      }
+    }
+  }
+
+  // TURN DETECTION (RIGHT)
+  if (turn_right)
+  {
+    if (is_end_of_corridor)
+    {
+      pot_found = pot_found || ps[0] > 800 || ps[15] > 800;
+      if (h < 0.3)
+      {
+        is_end_of_corridor = false;
+        state = NAVIGATE;
+        pot_found = false;
+      }
+    }
+    else
+    {
+      bool out_cor = true;
+      for (int i = 0; i <= 7; ++i)
+      {
+        if (ps[i] > 850)
+        {
+          out_cor = false;
+          break;
+        }
+      }
+      if (out_cor && !at_final_stop)
+      {
+        state = TURNING;
+        is_end_of_corridor = true;
+      }
+    }
+  }
+
+  // EXECUTE STATE
+  switch (state)
+  {
+  case NAVIGATE:
+    navigateBehavior(ps, vel_left, vel_right, pose);
+    break;
+  case TURNING:
+    turningBehavior(ps, vel_left, vel_right, pose, turn_left, turn_right, pot_found, search_pot, search_pot_counter, search_pot_duration);
+    break;
+  case EMERGENCY_BACKUP:
+    emergencyBackupBehavior(vel_left, vel_right);
+    break;
+  case STRAIGHT:
+    straightBehavior(vel_left, vel_right);
+    break;
+  case STOP:
+    stopBehavior(vel_left, vel_right);
+    break;
+  default:
+    vel_left = vel_right = 0.0;
+    break;
+  }
+
+  if (VERBOSE_STATE_NAME)
+    std::cout << "Current FSM state: " << getStateName(state) << std::endl;
+
+  return false;
 }
--- controllers/controller/braitenberg.hpp
+++ controllers/controller/braitenberg.hpp
@@ -5,10 +5,6 @@
 #include <iostream>
 #include <string>
 
-#define VERBOSE_STATE_NAME false
-#define VERBOSE_EMERGENCY_BACKUP false
-#define VERBOSE_TURN false
-
 /*
 
 Pioneer sensor layout:
@@ -33,61 +29,21 @@ Response: >5m -> 0, 0m -> 1024
  * @param      vel_left     The left velocity
  * @param      vel_right    The right velocity
  */
-enum RobotState
-{
-    NAVIGATE,
-    TURNING,
-    EMERGENCY_BACKUP,
-    STRAIGHT,
-    STOP_2
-};
-const char *getStateName(RobotState state)
-{
-    switch (state)
-    {
-    case NAVIGATE:
-        return "NAVIGATE";
-    case TURNING:
-        return "TURNING";
-    case EMERGENCY_BACKUP:
-        return "EMERGENCY_BACKUP";
-    default:
-        return "UNKNOWN";
-    }
-}
 
-void braitenberg(double *ps, double &vel_left, double &vel_right, double pose[4])
+void braitenberg(double *ps, double &vel_left, double &vel_right)
 {
 
     // TODO: implement your Braitenberg algorithm here
 
-    static RobotState state = NAVIGATE;
-    static RobotState prev_state = NAVIGATE;
-    static int backup_counter = 0;
-    const int backup_duration = 70;
-    static bool is_emergency = false;
-    static bool turn_right = false;
-    static bool turn_left = true;
-    static bool no_turn = true;
-    static bool is_end_of_corridor = false;
-    static bool pot_found = false;
-
-    /*     double braitenberg_coefficients[16][2] = {
-            {-0.6, -0.2}, {-0.5, -0.2}, {-0.4, -0.2}, {-0.3, -0.1},   // Front-left
-            {-0.1, -0.3}, {-0.2, -0.4}, {-0.2, -0.5}, {-0.2, -0.6},   // Front-right
-            {0.0,  0.0},  {0.0,  0.0},  {0.0,  0.0},  {0.0,  0.0},    // Rear-right
-            {0.0,  0.0},  {0.0,  0.0},  {0.0,  0.0},  {0.0,  0.0}};     // Rear-left
-     */
-
     double braitenberg_coefficients[16][2] = {
-        {0.0, -0.0}, // 0: slight steer
+        {0.1, -0.0}, // 0: slight steer
         {0.2, -0.4}, // 1
         {0.2, -0.3}, // 2
         {0.2, -0.3}, // 3
         {-0.3, 0.2}, // 4
         {-0.3, 0.2}, // 5
         {-0.4, 0.2}, // 6
-        {-0.0, 0.0}, // 7
+        {-0.0, 0.1}, // 7
         {0.0, 0.0},  // rear sensors ignored
         {0.0, 0.0},
         {0.0, 0.0},
@@ -99,310 +55,28 @@ void braitenberg(double *ps, double &vel_left, double &vel_right, double pose[4]
 
     const double base_speed = 1.0;
 
-    // heading variable
-    double h = pose[2];
-    double x = pose[0];
-    double y = pose[1];
-
-    if (x < 1)
-    {
-        turn_right = true;
-        turn_left = false;
-    }
-    if (x > 5)
-    {
-        turn_right = false;
-        turn_left = true;
-    }
-    else
-    {
-        no_turn = true;
-    }
-
-    ////////////////////// STOP ///////////////////////////////////////
-
-
-    bool at_final_stop = (y > 3.5 && y < 4.5 && x < 0);  // adapt to map
-    static int stop_counter = 0;
-    const int stop_confirm_duration = 150;
-
-    if (at_final_stop) {
-        stop_counter++;
-        if (stop_counter > stop_confirm_duration) {
-            state = STOP_2;
-            return;
-        }
-    } else {
-        stop_counter = 0;
-    }
-
-
-
-
-    /////////////////turn left ///////////////////////////
-    if (turn_left)
-    {
-        if (is_end_of_corridor)
-        {
-            pot_found = pot_found || ps[0] > 800 || ps[15] > 800;
-
-            if (h > 3)
-            {
-                is_end_of_corridor = false;
-                state = NAVIGATE;
-                pot_found = false;
-            }
-        }
-        else
-        {
-            bool out_cor = true;
-            for (int i = 0; i <= 7; ++i)
-            {
-
-                if (ps[i] > 850)
-                {
-                    out_cor = false;
-                    break;
-                }
-            }
-            if (out_cor && !at_final_stop)
-            {
-                state = TURNING;
-                is_end_of_corridor = true;
-            }
-        }
-    }
-
-    /////////////////turn right ///////////////////////////
-    if (turn_right)
-    {
-        if (is_end_of_corridor)
-        {
-
-            if (h < 0.3)
-            {
-                is_end_of_corridor = false;
-                state = NAVIGATE;
-            }
-        }
-        else
-        {
-            bool out_cor = true;
-            for (int i = 0; i <= 7; ++i)
-            {
-
-                if (ps[i] > 850)
-                {
-                    out_cor = false;
-                    break;
-                }
-            }
-            if (out_cor && !at_final_stop)
-            {
-                state = TURNING;
-                is_end_of_corridor = true;
-            }
-        }
-    }
-
-    ///////////////////////////////////////////////////////////////////////////
-
     double influence_sum_l = 0.0;
     double influence_sum_r = 0.0;
 
-    ////////////////emergency///////////////////////////////////
+    vel_left = base_speed;
+    vel_right = base_speed;
 
-    if (is_emergency)
+    for (int i = 0; i < 8; ++i)
     {
-        backup_counter++;
-        state = EMERGENCY_BACKUP;
-        if (VERBOSE_EMERGENCY_BACKUP)
-        {
-            printf("adf %d\n", backup_counter);
-        }
+        if (ps[i] < 900)
+            ps[i] = 800;                                                // Ignore far sensors
+        double influence = pow(((double)ps[i] - 800) / (960 - 800), 4); // Stronger nonlinear amplification
 
-        if (backup_counter > backup_duration)
-        {
-            state = prev_state;
-            backup_counter = 0;
-            is_emergency = false;
-        }
-    }
-    else
-    {
-        for (int i = 1; i <= 6; ++i)
-        {
-            if (ps[i] > 1005)
-            {
-                prev_state = state;
-                state = EMERGENCY_BACKUP;
-                is_emergency = true;
+        influence_sum_l += std::abs(braitenberg_coefficients[i][0] * influence);
+        influence_sum_r += std::abs(braitenberg_coefficients[i][1] * influence);
 
-                break;
-            }
-        }
+        vel_left += braitenberg_coefficients[i][0] * influence;
+        vel_right += braitenberg_coefficients[i][1] * influence;
     }
 
-    if (state == TURNING)
-    {
-        if (x > 0 && x < 5)
-        {
-            state = NAVIGATE;
-            is_end_of_corridor = false;
-        }
-    }
-
-    /////////////////////////////////////////               /////////////////////////////////////////
-    /////////////////////////////////////////     SWITCH    /////////////////////////////////////////
-    /////////////////////////////////////////               /////////////////////////////////////////
-
-    switch (state)
-    {
-    case NAVIGATE:
-
-        vel_left = base_speed;
-        vel_right = base_speed;
-
-        for (int i = 0; i < 8; ++i)
-        {
-            if (ps[i] < 940)
-                ps[i] = 800;                                                // Ignore far sensors
-            double influence = pow(((double)ps[i] - 800) / (960 - 800), 4); // Stronger nonlinear amplification
-
-            influence_sum_l += std::abs(braitenberg_coefficients[i][0] * influence);
-            influence_sum_r += std::abs(braitenberg_coefficients[i][1] * influence);
-
-            vel_left += braitenberg_coefficients[i][0] * influence;
-            vel_right += braitenberg_coefficients[i][1] * influence;
-        }
-
-        // Optionally normalize to avoid overcorrection
-        if (influence_sum_l > 1.0)
-            vel_left /= influence_sum_l;
-        if (influence_sum_r > 1.0)
-            vel_right /= influence_sum_r;
-
-        // if (vel_left < 0) vel_left = 0;
-        // if (vel_right < 0) vel_right = 0;
-        break;
-
-    case TURNING:
-
-        if (turn_left)
-        {
-            if (VERBOSE_TURN)
-            {
-
-                printf("\nturnleft");
-            }
-
-            double target = 990; // target value for ~0.5m distance
-            double sensor_left = std::max(ps[0], ps[15]);
-            double error = sensor_left - target;
-            double Kp = 0.005;
-            double correction = Kp * error;
-
-            if (VERBOSE_TURN)
-            {
-
-                printf("\ncorrection = %f\n", correction);
-            }
-
-            if (pot_found && sensor_left < 850)
-            {
-                vel_left = 0.5;
-                vel_right = 0.5;
-            }
-            else if (ps[0] < ps[15])
-            {
-                double correction_val = 0.2 + correction / 2;
-
-                vel_left = std::min(std::max(correction_val, -3.), 3.);
-                vel_right = 0.7 - correction;
-            }
-            else if (ps[0] > ps[15])
-            {
-                vel_left = 0.3;
-                vel_right = 0.5 - correction;
-            }
-        }
-
-        if (turn_right)
-        {
-            if (VERBOSE_TURN)
-            {
-
-                printf("\nturnright");
-            }
-
-            double target = 990; // target value for ~0.5m distance
-            double sensor_right = std::max(ps[7], ps[8]);
-            double error = sensor_right - target;
-            double Kp = 0.005;
-            double correction = Kp * error;
-
-            if (VERBOSE_TURN)
-            {
-
-                printf("\ncorrection = %f\n", correction);
-            }
-
-            if (sensor_right < 850)
-            {
-                vel_left = 0.5;
-                vel_right = 0.5;
-            }
-            else if (ps[7] < ps[8])
-            {
-                vel_right = 0.2 + correction;
-                vel_left = 0.7 - correction;
-            }
-            else if (ps[7] > ps[8])
-            {
-                vel_right = 0.3;
-                vel_left = 0.5 - correction;
-            }
-        }
-
-        break;
-
-    case EMERGENCY_BACKUP:
-        vel_left = -0.5;
-        vel_right = -0.5;
-
-        break;
-
-    case STRAIGHT:
-
-        vel_left = 0.5;
-        vel_right = 0.5;
-        break;
-
-    
-
-    case STOP_2:
-
-        vel_left = 0.0;
-        vel_right = 0.0;
-        break;
-    }
-
-    if (VERBOSE_STATE_NAME)
-    {
-        std::cout << "State name: " << getStateName(state) << std::endl;
-    }
-
-    /*     bool danger_close = false;
-        for (int i = 0; i <= 7; ++i) {
-            if (ps[i] > 1000) danger_close = true;
-        }
-        if (!danger_close) {
-            vel_left += 0.2;
-            vel_right += 0.2;
-        } */
-
-    // if (vel_left > 1.0) vel_left = 1.0;
-    // if (vel_left < 0) vel_left = 0;
-    // if (vel_right > 1.0) vel_right = 1.0;
-    // if (vel_right < 0) vel_right = 0;
+    // Optionally normalize to avoid overcorrection
+    if (influence_sum_l > 1.0)
+        vel_left /= influence_sum_l;
+    if (influence_sum_r > 1.0)
+        vel_right /= influence_sum_r;
 }
