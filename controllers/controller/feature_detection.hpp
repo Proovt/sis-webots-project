cd42deb2c82ad9e99ecff983bc23f5dfa2b5d78e
Nico Mike Lüthi <nluethi@ENAC-SSIE-UL010.intranet.epfl.ch>
2025-05-29 11:43:07 +0200

    [SENSOR] Static vectors and constants

--- controllers/controller/feature_detection.hpp
+++ controllers/controller/feature_detection.hpp
@@ -3,33 +3,37 @@
 #include "kalman.hpp"
 #include "serial.hpp"
 
-#define VERBOSE_SIGNAL_RADIUS false // Prints the calculated distance for a given signal strength
+#define VERBOSE_SIGNAL_RADIUS false // Prints the calculated horizontal distance from the sensor node to the robot for a given signal strength
 
-#define PROPORTIONALITY_FACTOR 1.08 // [-]
-#define SENSOR_NODE_DST 0.723 // [m] distance between light sensor and sensor node
-#define SENSOR_NODE_VARIANCE 0.01 // [m^2] (empirical)
+#define PROPORTIONALITY_FACTOR 1.08  // [-] (empirical)
+#define SENSOR_NODE_DST 0.723        // [m] Distance between light sensor and sensor node
+#define SENSOR_NODE_BIAS_FACTOR 0.01 // [m] Bias term factor (empirical)
+#define SENSOR_NODE_VARIANCE 0.01    // [m^2] (empirical)
+
+static Vec2D last_pos, sensor_pos, last_pos_direction, bias, estimated_pos;
 
 void handle_sensor_node(double data[PACKET_SIZE], double signal_strength, Vec &mu, Mat &Sigma)
 {
-    double d_sqr = PROPORTIONALITY_FACTOR / signal_strength;
-    double radius = sqrt(d_sqr - SENSOR_NODE_DST * SENSOR_NODE_DST);
+    double dst_sqr = PROPORTIONALITY_FACTOR / signal_strength;
+    double radius = sqrt(dst_sqr - SENSOR_NODE_DST * SENSOR_NODE_DST);
 
     if (VERBOSE_SIGNAL_RADIUS)
     {
-        printf("Radius: %f, Signal strength: %f\n", radius, signal_strength);
+        printf("Horizontal Distance: %f m, Signal strength: %f\n", radius, signal_strength);
     }
 
-    Vec2D last_pos(mu(0), mu(1));
-    Vec2D sensor_pos(data[1], data[2]);
+    last_pos << mu(0), mu(1);
+    sensor_pos << data[1], data[2];
 
-    Vec2D diff = last_pos - sensor_pos;
-    diff.normalize();
+    // Calculate the direction of the last position from the sensor node
+    last_pos_direction = last_pos - sensor_pos;
+    last_pos_direction.normalize();
 
-    Vec2D bias = Vec2D::Ones() * .01;
-    bias[0] *= cos(mu(2));
-    bias[1] *= sin(mu(2));
+    // Calculate bias term in forward direction
+    bias << cos(mu(2)), sin(mu(2));
+    bias *= SENSOR_NODE_BIAS_FACTOR;
 
-    Vec2D estimated_pos = sensor_pos + diff * radius + bias;
+    estimated_pos << sensor_pos + last_pos_direction * radius + bias;
 
     update_step_sensor_node(mu, estimated_pos, Sigma, SENSOR_NODE_VARIANCE);
 }
