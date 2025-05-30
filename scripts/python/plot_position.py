a3ac6185c7b53b12fb145141e6bf789873321c38
Nico Mike Lüthi <nluethi@ENAC-SSIE-UL013.intranet.epfl.ch>
2025-05-30 10:56:45 +0200

    [SCRIPTS] Relative import and changed plot axis names

--- scripts/python/plot_position.py
+++ scripts/python/plot_position.py
@@ -1,12 +1,16 @@
+import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+
+from pathlib import Path
+
+cur_dir = Path(sys.path[0])
 
 # Define file paths
-truth_path = os.path.join("..", "..", "controllers", "supervisor", "data", "ground_truth.csv")
-odo_path = os.path.join("..", "..", "controllers", "controller", "data", "odo.csv")
-sigma_path = os.path.join('..', '..', 'controllers', "controller", "data", "odo_sigma.csv")
+truth_path = cur_dir.joinpath("..", "..", "controllers", "supervisor", "data", "ground_truth.csv")
+odo_path = cur_dir.joinpath("..", "..", "controllers", "controller", "data", "odo.csv")
+sigma_path = cur_dir.joinpath('..', '..', 'controllers', "controller", "data", "odo_sigma.csv")
 
 # Load data
 ground_truth = pd.read_csv(truth_path)
@@ -30,7 +34,6 @@ odo_heading = odometry['heading'].to_numpy()
 plt.figure(figsize=(10, 6))
 plt.plot(gt_x, gt_y, label='Ground Truth', color='blue', linewidth=2)
 plt.plot(odo_x, odo_y, label='Odometry Estimate', color='orange', linestyle='--', linewidth=2)
-
 plt.title('Ground Truth vs Odometry Trajectory')
 plt.xlabel('X Position [m]')
 plt.ylabel('Y Position [m]')
@@ -38,10 +41,9 @@ plt.legend()
 plt.grid(True)
 plt.axis('equal')
 plt.tight_layout()
-
 plt.show()
 
-# Error
+""" x and y error """
 # Merge on 'time' to align matching timestamps
 merged_all = pd.merge(odometry, ground_truth, on='time', suffixes=('_odo', '_gt'))
 time = merged_all['time'].to_numpy()
@@ -54,19 +56,19 @@ error_y = (merged_all['y_odo'] - merged_all['y_gt']).abs().to_numpy()
 sigma2_x = uncertainty['x'].to_numpy()
 sigma2_y = uncertainty['y'].to_numpy()
 
-# Plotting
+# Figure 3: X and Y error
 plt.figure(figsize=(12, 6))
 plt.plot(time, error_x, label='X Error', color='blue')
 plt.plot(time, np.sqrt(sigma2_x), label='X Uncertainty', color='blue', linestyle='--')
 plt.plot(time, error_y, label='Y Error', color='orange')
 plt.plot(time, np.sqrt(sigma2_y), label='Y Uncertainty', color='orange', linestyle='--')
 plt.title('Position Error vs Uncertainty')
-plt.xlabel('Time (s)')
-plt.ylabel('Error (m)')
+plt.xlabel('Time [s]')
+plt.ylabel('Error [m]')
 plt.legend()
 plt.grid(True)
+plt.tight_layout()
 plt.show()
-
 
 # Calculate angular error in rad
 heading_diff = (merged_all['heading_odo'] - merged_all['heading_gt']) % (2 * np.pi)
@@ -77,15 +79,14 @@ heading_error *= 180 / np.pi # Convert to degrees
 sigma2_heading = uncertainty['heading'].to_numpy()
 sigma_heading = np.sqrt(sigma2_heading) * 180 / np.pi # Convert to degrees
 
-# Plot Heading error
+# Figure 3: Heading error
 plt.figure(figsize=(12, 6))
 plt.plot(time, heading_error, label='Heading Error', color='dodgerblue')
 plt.plot(time, sigma_heading, label='Heading Uncertainty', color='dodgerblue', linestyle='--')
 plt.title('Heading Error vs Uncertainty')
 plt.xlabel('Time [s]')
-plt.ylabel('Heading Error [°]')
+plt.ylabel('Heading Error [$\degree$]')
 plt.legend()
 plt.grid(True)
-
 plt.tight_layout()
 plt.show()
